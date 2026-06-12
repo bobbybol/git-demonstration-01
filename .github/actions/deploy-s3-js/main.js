6b074236cae4f113bb044ecf9c2439a774b43fb6
Bobby Bol <studio.innua@gmail.com>
2026-06-12 10:35:45 +0200

    deploying dist files to S3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,8 +1,17 @@
 import * as core from "@actions/core"; 
-import * as github from "@actions/github"; 
+// import * as github from "@actions/github"; 
 import * as exec from "@actions/exec";
 
 function run() {
+  // 1) Get the input values
+  const bucket = core.getInput('bucket', { required: true });
+  const region = core.getInput('bucket-region', { required: true });
+  const folder = core.getInput('dist-folder', { required: true });
+
+  // 2) Upload files to S3
+  const s3Uri = `s3://${ bucket }`;
+  exec.exec(`aws s3 sync ${ folder } ${ s3Uri } --region ${ region }`);
+
   core.notice('Poepjes from JS Action 🍑💨')
 }
 
